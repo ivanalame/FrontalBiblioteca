2afd56613db9f20f5623187cd50356bc5d817923
ivan <alumno.399451@ies-azarquiel.es>
2023-03-31 11:23:41 +0200

    Diseño de gestion

---
+++ BibliotecaModelos/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModelos.Entities
+{
+    public class Entities
+    {
+        public class User
+        {
+            public int Id { get; set; }
+            public int usuario { get; set; }
+            public int password { get; set; }
+
+        }
+
+        public class Libros
+        {
+            public int Id { get; set; }
+            public int Titulo { get; set; }
+            public int Editorial { get; set; }
+            public bool reservado;
+
+        }
+    }
+}
